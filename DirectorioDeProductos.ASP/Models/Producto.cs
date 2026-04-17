ab31d4d9f9161eab3f31e50a7b04cda1b0a8d1f8
JOSETH.ROJAS@ucr.ac.cr <JOSETH.ROJAS@ucr.ac.cr>
2026-04-17 17:19:17 -0600

    Agregar archivos de proyecto.

---
+++ DirectorioDeProductos.ASP/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DirectorioDeProductos.ASP.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Range(0.01, 999999, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria")]
+        public string Categoria { get; set; }
+    }
+}
